d6b10323feb872dc66b7c88bd259f22cabb8f8a4
RenHayashi <69228985+RenHayashi@users.noreply.github.com>
2020-08-05 14:07:29 +0900

    最初のバージョン

---
+++ coin.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#include<math.h>
+
+int main(){
+  int coin = 0,total_H = 0,total_T = 0;
+  int i = 0;
+  
+  srand(time(NULL));
+
+  for (i = 0;i < 3;i++){
+  coin = (int)rand() %2;
+
+  if (coin == 0){
+    printf("Round %d: Tails\n",i);
+    total_T++;
+  }
+
+  else{
+    printf("Round %d: Heads\n",i);
+    total_H++;
+  }
+  }
+
+  printf("\nHeads: %d, Tails: %d\n",total_H,total_T);
+  
+  return 0;
+}
